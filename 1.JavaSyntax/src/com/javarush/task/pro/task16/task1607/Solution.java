5a86744374725a3e40756dc9330da44e37e02f8d
osadasami <osadasami@gmail.com>
2022-09-03 18:38:30 +0500

    update

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task16/task1607/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.pro.task16.task1607;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+/* 
+Освоение нового API
+*/
+
+public class Solution {
+
+  public static void main(String[] args) {
+    System.out.println(nowExample());
+    System.out.println(ofExample());
+    System.out.println(ofYearDayExample());
+    System.out.println(ofEpochDayExample());
+  }
+
+  static LocalDate nowExample() {
+    return LocalDate.now();
+  }
+
+  static LocalDate ofExample() {
+    return LocalDate.of(2020, Month.SEPTEMBER, 12);
+  }
+
+  static LocalDate ofYearDayExample() {
+    return LocalDate.ofYearDay(2020, ofExample().getDayOfYear());
+  }
+
+  static LocalDate ofEpochDayExample() {
+    return LocalDate.ofEpochDay(ofExample().toEpochDay());
+  }
+}
