8cc08c58838a9a41a9384aa9e1115e9f095432e0
osadasami <osadasami@gmail.com>
2022-09-06 14:38:41 +0500

    update

---
+++ 1.JavaSyntax/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+
+  }
+
+  public interface CanFly {
+    void method1();
+  }
+
+  public interface CanRun {
+    void method1();
+  }
+
+  public interface CanSwim {
+    void method1();
+  }
+
+}
