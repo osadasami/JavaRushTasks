b2bba37bcbca0f33e8c101b9beb028ca62a59c93
osadasami <osadasami@gmail.com>
2022-09-06 12:54:43 +0500

    update

---
+++ 1.JavaSyntax/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+  public static void main(String[] args) {
+    print(1);
+    print(new Integer(1));
+  }
+
+  public static void print(int n) {
+  }
+
+  public static void print(Integer n) {
+  }
+}
