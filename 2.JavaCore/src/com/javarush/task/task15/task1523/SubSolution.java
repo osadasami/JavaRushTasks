d7430eea48cb43fe17ada52d32a2ecb4df36daba
osadasami <osadasami@gmail.com>
2022-09-11 12:18:13 +0500

    update

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+
+  SubSolution() {
+  }
+
+  public SubSolution(short i) {
+    super(i);
+  }
+
+  protected SubSolution(int i) {
+    super(i);
+  }
+
+}
