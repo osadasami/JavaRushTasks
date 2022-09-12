61896a1590efb6fc3c090ec15eca843799773eb6
osadasami <osadasami@gmail.com>
2022-09-12 14:18:40 +0500

    update

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/ImageReaderFactory.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task16.task1631;
+
+import com.javarush.task.task16.task1631.common.BmpReader;
+import com.javarush.task.task16.task1631.common.ImageReader;
+import com.javarush.task.task16.task1631.common.ImageTypes;
+import com.javarush.task.task16.task1631.common.JpgReader;
+import com.javarush.task.task16.task1631.common.PngReader;
+
+public class ImageReaderFactory {
+  public static ImageReader getImageReader(ImageTypes type) {
+    if (type == ImageTypes.BMP) {
+      return new BmpReader();
+    } else if (type == ImageTypes.JPG) {
+      return new JpgReader();
+    } else if (type == ImageTypes.PNG) {
+      return new PngReader();
+    } else {
+      throw new IllegalArgumentException("Неизвестный тип картинки");
+    }
+  }
+}
